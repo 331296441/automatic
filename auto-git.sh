8ebaae4117fd5e4f0868988c6c9632af01805f8c
OneCloudHome <mujj.me@qq.com>
2023-09-16 15:17:15 +0800

    自动提交 by onecloudHome 2023-09-16 15:17:14

--- auto-git.sh
+++ auto-git.sh
@@ -9,7 +9,7 @@ git remote remove origin || true
 git remote add github git@github.com:331296441/automatic.git  || true
 git remote add gitee git@gitee.com:qq331296441/automatic.git || true
 # git remote add origin git@gitee.com:qq331296441/automatic.git || true
-git branch --set-upstream-to=gitee/master
+git branch --set-upstream-to=gitee/master || true
 
 # 获取当前脚本所在目录的绝对路径
 SCRIPT=$(readlink -f "$0")
