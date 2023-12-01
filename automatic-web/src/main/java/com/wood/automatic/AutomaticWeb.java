3b595dcf7b4aeb8d30d28a31136b53538e7a83b8
D410c <331296441@qq.com>
2023-12-01 15:35:08 +0800

    auto commit by linaro-alip 2023-12-01 15:34:21

--- automatic-web/src/main/java/com/wood/automatic/AutomaticWeb.java
+++ automatic-web/src/main/java/com/wood/automatic/AutomaticWeb.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutomaticSWeb {
+public class AutomaticWeb {
     public static void main(String[] args) {
         SpringApplication.run(AutomaticWeb.class, args);
         System.out.println("启动成功");
