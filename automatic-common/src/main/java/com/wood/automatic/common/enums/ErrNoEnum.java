9859ce307828ea94ef86fb0adf5583e1bcb61cde
HP EliteDesk 800 G3 DM <331296441@qq.com>
2024-03-18 20:39:06 +0800

    auto commit by EliteDesk 2024-03-18 20:39:06

--- automatic-common/src/main/java/com/wood/automatic/common/enums/ErrNoEnum.java
+++ automatic-common/src/main/java/com/wood/automatic/common/enums/ErrNoEnum.java
@@ -6,6 +6,13 @@ public enum ErrNoEnum{
     SUCCESS_REQUEST(200,"请求成功"),
     ERROR_REQUEST(400,"请求失败"),
     ERROR_NOT_FOUND(404,"请求资源不存在"),
-    
+    ERROR_SERVER(500,"服务器错误"),
+;
+    private int code;
+    private String msg;
+    ErrNoEnum(int code,String msg){
+        this.code = code;
+        this.msg = msg;
+    }
 
 }
