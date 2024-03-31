d89cf092be5cea2b9ea2a064772bd3a8ef603497
331296441 <331296441@qq.com>
2024-03-31 09:17:31 +0800

    获取本机IP能力

--- automatic-common/src/main/java/com/wood/automatic/common/utils/IPUtil.java
+++ automatic-common/src/main/java/com/wood/automatic/common/utils/IPUtil.java
@@ -1,6 +1,30 @@
 package com.wood.automatic.common.utils;
 
 
-public class IPUtil{
-    
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class IPUtil {
+
+    public static String getLocalIPAddress() throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+                if (!inetAddress.isLoopbackAddress() && inetAddress.getAddress().length == 4) {
+                    return inetAddress.getHostAddress();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws SocketException {
+        String localIPAddress = IPUtil.getLocalIPAddress();
+        System.out.println(localIPAddress);
+    }
 }
