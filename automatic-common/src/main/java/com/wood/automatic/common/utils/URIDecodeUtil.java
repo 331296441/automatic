2a5b6121345c2835115d119020bd8158cbe0296e
331296441 <331296441@qq.com>
2024-04-08 20:34:26 +0800

    编码

--- automatic-common/src/main/java/com/wood/automatic/common/utils/URIDecodeUtil.java
+++ automatic-common/src/main/java/com/wood/automatic/common/utils/URIDecodeUtil.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 @Slf4j
 public class URIDecodeUtil {
@@ -20,7 +21,18 @@ public class URIDecodeUtil {
         try {
             s = URLDecoder.decode(secureString, Constants.UTF8);
         }catch (UnsupportedEncodingException e){
-            log.error("{},{}",ErrNoEnum.ERROR_DECODE.getMsg(),e.toString());
+            log.error("{}:{}",ErrNoEnum.ERROR_DECODE.getMsg(),e.toString());
+        }
+        return s;
+    }
+
+    public static String encodeUri(String unSecureString){
+        String s = "";
+
+        try{
+            s = URLEncoder.encode(unSecureString,Constants.UTF8);
+        }catch(UnsupportedEncodingException e){
+            log.error("{}:{}",ErrNoEnum.ERROR_ENCODE.getMsg(),e.toString());
         }
         return s;
     }
