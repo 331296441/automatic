65301901e716acd1ebf2c257f0cbd79c68f02677
331296441 <331296441@qq.com>
2024-05-31 10:58:58 +0800

    生产者更新

--- automatic-web/src/main/java/com/wood/automatic/rockermq/RocketMQProducer.java
+++ automatic-web/src/main/java/com/wood/automatic/rockermq/RocketMQProducer.java
@@ -2,14 +2,18 @@ package com.wood.automatic.rockermq;
 
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service
 public class RocketMQProducer {
     @Resource
     private RocketMQTemplate rocketMQTemplate;
     private String topicName="test-message";
 
+    @Scheduled(cron ="* * * * * ?")
     public void sendMessage(){
         rocketMQTemplate.convertAndSend(topicName,"Hello World");
         rocketMQTemplate.send(topicName, MessageBuilder.withPayload("Hello World! from spring").build());
