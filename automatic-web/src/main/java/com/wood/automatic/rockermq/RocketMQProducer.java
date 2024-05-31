a1ad61c2e190bf360a397cffc1100808c49eb7e5
331296441 <331296441@qq.com>
2024-05-31 10:43:13 +0800

    发送功能

--- automatic-web/src/main/java/com/wood/automatic/rockermq/RocketMQProducer.java
+++ automatic-web/src/main/java/com/wood/automatic/rockermq/RocketMQProducer.java
@@ -1,3 +1,17 @@
+package com.wood.automatic.rockermq;
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.messaging.support.MessageBuilder;
+
+import javax.annotation.Resource;
+
 public class RocketMQProducer {
-    
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+    private String topicName="test-message";
+
+    public void sendMessage(){
+        rocketMQTemplate.convertAndSend(topicName,"Hello World");
+        rocketMQTemplate.send(topicName, MessageBuilder.withPayload("Hello World! from spring").build());
+    }
 }
