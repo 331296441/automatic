fe2b3564a5105e0d3a3e7e67bcde800fc369b816
EliteDesk <331296441@qq.com>
2024-06-18 14:49:07 +0800

    脚本提交 by EliteDesk 2024-06-18 14:49:07, 提交内容：脚本定时提交

--- sql/test_case.sql
+++ sql/test_case.sql
@@ -6,6 +6,7 @@ CREATE TABLE `test_case` (
     `case_type` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '用例类型',
     `create_user` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '创建人',
     `update_user` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '更新人',
-    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP C
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT ON UPDATE CURRENT_TIMESTAMP '更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '测试用例表';
--- sql/test_plan.sql
+++ sql/test_plan.sql
@@ -4,5 +4,9 @@ CREATE TABLE `test_plan` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
     `biz_name` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '所属业务线',
     `project_name` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '所属项目',
+    `create_user` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '创建人',
+    `update_user` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '更新人',
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT ON UPDATE CURRENT_TIMESTAMP '更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '测试计划表';
