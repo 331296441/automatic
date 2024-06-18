73e3f37cbf79a8a8956eb897a3552f607202e6ff
EliteDesk <331296441@qq.com>
2024-06-18 14:34:07 +0800

    脚本提交 by EliteDesk 2024-06-18 14:34:07, 提交内容：脚本定时提交

--- sql/test_case.sql
+++ sql/test_case.sql
@@ -1,6 +1,7 @@
 drop table if exists `test_case`;
 CREATE TABLE `test_case`
 (
-    `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT ''
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    PRIMARY KEY (`id`)
 ) ENGINE =InnoDB
 DEFAULT CHARSET = utf8mb4 COMMENT='测试用例表';
