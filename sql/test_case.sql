9418d7efa3f6afea7b4cf89ca751a4dc4df74166
EliteDesk <331296441@qq.com>
2024-06-18 14:46:06 +0800

    脚本提交 by EliteDesk 2024-06-18 14:46:06, 提交内容：脚本定时提交

--- sql/test_case.sql
+++ sql/test_case.sql
@@ -4,5 +4,6 @@ CREATE TABLE `test_case` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
     `case_name` VARCHAR(512) NOT NULL DEFAULT '' COMMENT '用例名称',
     `case_type` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '用例类型',
+    `create_user` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '创建人'
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '测试用例表';
