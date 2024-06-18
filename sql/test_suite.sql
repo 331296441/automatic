947d9bf8509a04562aca6eb25426c01aeec73802
EliteDesk <331296441@qq.com>
2024-06-18 14:44:06 +0800

    脚本提交 by EliteDesk 2024-06-18 14:44:06, 提交内容：脚本定时提交

--- sql/test_suite.sql
+++ sql/test_suite.sql
@@ -2,5 +2,7 @@ drop table if exists `test_suite`;
 
 CREATE TABLE `test_suite` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `biz_name` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '所属业务线',
+    `project_name` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '所属项目',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '测试套表';
