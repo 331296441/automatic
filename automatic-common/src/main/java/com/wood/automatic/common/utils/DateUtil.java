87fde6e12d9a936d936aae09f8e9efef5d3556df
EliteDesk <331296441@qq.com>
2024-06-24 14:14:07 +0800

    脚本提交 by EliteDesk 2024-06-24 14:14:07, 提交内容：脚本定时提交

--- automatic-common/src/main/java/com/wood/automatic/common/utils/DateUtil.java
+++ automatic-common/src/main/java/com/wood/automatic/common/utils/DateUtil.java
@@ -1,6 +1,25 @@
 package com.wood.automatic.common.utils;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class DateUtil {
-    
+     // Date 转 LocalDateTime
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        if (date == null) {
+            return null;
+        }
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    // LocalDateTime 转 Date
+    public static Date localDateTimeToDate(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return null;
+        }
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        return Date.from(zonedDateTime.toInstant());
+    }
 
 }
