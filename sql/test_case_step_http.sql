529d150820086f8dbb749619a982e1fecd7befa0
EliteDesk <331296441@qq.com>
2024-07-11 17:38:06 +0800

    脚本提交 by EliteDesk 2024-07-11 17:38:05, 提交内容：脚本定时提交

--- sql/test_case_step_http.sql
+++ sql/test_case_step_http.sql
@@ -4,7 +4,13 @@ CREATE TABLE `test_case_step_interface` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
     `step_type` int(2) not null comment '默认类型 0 HTTP接口 1 Web UI 2 API驱动  3移动端',
     `desc` varchar(255) DEFAULT NULL COMMENT '步骤描述',
-    `interface_url` VARCHAR
+    `interface_url` VARCHAR(255) DEFAULT NULL COMMENT '接口地址',
+    `interface_method` VARCHAR(255) DEFAULT NULL COMMENT '接口方法',
+    `interface_body` VARCHAR(255) DEFAULT NULL COMMENT '接口请求体',
+    `interface_headers` VARCHAR(255) DEFAULT NULL COMMENT '接口请求头',
+    `interface_params` VARCHAR(255) DEFAULT NULL COMMENT '接口请求参数',
+    `interface_assert` VARCHAR(255) DEFAULT NULL COMMENT '接口断言',
+    `interface_return` VARCHAR(255) DEFAULT NULL COMMENT '接口返回值',
 
         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '单一测试步骤';
