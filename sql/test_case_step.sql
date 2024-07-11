a33d09e772d20d3cad1a0ea57e7b7d54fa531245
EliteDesk <331296441@qq.com>
2024-07-11 17:13:07 +0800

    脚本提交 by EliteDesk 2024-07-11 17:13:07, 提交内容：脚本定时提交

--- sql/test_case_step.sql
+++ sql/test_case_step.sql
@@ -1,6 +1,6 @@
-drop table if exists `test_case_steps`;
+drop table if exists `test_case_step`;
 
-CREATE TABLE `test_case_steps` (
+CREATE TABLE `test_case_step` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
     `test_case_id` bigint unsigned NOT NULL COMMENT '测试用例id',
         PRIMARY KEY (`id`)
