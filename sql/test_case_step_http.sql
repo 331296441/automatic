c1d0f0b5b7a1f1ef0c8be96c607ec6af2d304601
EliteDesk <331296441@qq.com>
2024-07-11 17:41:06 +0800

    脚本提交 by EliteDesk 2024-07-11 17:41:06, 提交内容：脚本定时提交

--- sql/test_case_step_http.sql
+++ sql/test_case_step_http.sql
@@ -8,9 +8,11 @@ CREATE TABLE `test_case_step_interface` (
     `method` VARCHAR(255) DEFAULT NULL COMMENT '请求方法',
     `content_type` VARCHAR(255) DEFAULT NULL COMMENT 'HTTP内容类型',
     `headers` text COMMENT '接口请求头',
-    `params` VARCHAR(255) DEFAULT NULL COMMENT '接口请求参数',
-    `assert` VARCHAR(255) DEFAULT NULL COMMENT '接口断言',
-    `interface_return` VARCHAR(255) DEFAULT NULL COMMENT '接口返回值',
+    `params` text COMMENT '接口请求参数',
+    `assert` text COMMENT '接口断言',
+    `return` text COMMENT '接口返回值',
+    `save` TEXT COMMENT '接口返回值保存',
+    `test_case_id` bigint unsigned NOT NULL COMMENT '所属测试用例',
 
         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '单一测试步骤';
